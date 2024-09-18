1b42d284792267e1731776796f6e91d39814f11a
Mohan N V <iam.mohannv@gmail.com>
2024-09-18 12:26:42 +0530

    Initial commit

---
+++ src/main/java/com/wtg/empdept_api/EmployeeDepartmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.wtg.empdept_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepartmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDepartmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtg/empdept_api/EmployeeDepartmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtg.empdept_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDepartmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
