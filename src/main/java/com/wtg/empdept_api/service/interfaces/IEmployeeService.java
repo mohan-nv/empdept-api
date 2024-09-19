1dd7b312545b066cdc2026896f35f49802225883
Mohan N V <iam.mohannv@gmail.com>
2024-09-20 00:40:46 +0530

    Employee CRUD APIs

--- src/main/java/com/wtg/empdept_api/service/interfaces/IEmployeeService.java
+++ src/main/java/com/wtg/empdept_api/service/interfaces/IEmployeeService.java
@@ -1,4 +1,20 @@
 package com.wtg.empdept_api.service.interfaces;
 
+import com.wtg.empdept_api.dao.entity.Employee;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.security.InvalidParameterException;
+import java.util.List;
+
 public interface IEmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    Employee createEmployee(Employee employee) throws UnsupportedOperationException, InvalidParameterException;
+
+    Employee getEmployeeById(Long id) throws EntityNotFoundException;
+
+    Employee updateEmployee(Employee employeeRequest) throws EntityNotFoundException, InvalidParameterException;
+
+    Boolean deleteEmployee(Long id) throws EntityNotFoundException;
 }
